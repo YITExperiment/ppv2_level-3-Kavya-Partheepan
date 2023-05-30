98e87263d705c29f4b2ac036533dcd20c8a2cdb1
Kavya-Partheepan <134903846+Kavya-Partheepan@users.noreply.github.com>
2023-05-30 22:50:52 +0530

    Add files via upload

---
+++ myfirstprogram 2.py
@@ -0,0 +1,19 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/Kodees/Desktop/PPv_3/No 1/myfirstprogram.py ========
+Hello, World!
+>>> 
+========= RESTART: C:/Users/Kodees/Desktop/PPv_3/No 1/myfirstprogram.py ========
+Hello, World!
+What is your name? Kavya
+Hello  Kavya
+>>> age=13
+>>> print(age)
+13
+>>> weight=39
+>>> print(weight)
+39
+>>> height=146
+>>> print(height)
+146
